3ae6877ba0985ba061f77e0381c1aa4ab7602193
Cuonglv97 <levietcuong0297@gmail.com>
2022-01-28 00:54:58 +0700

    Update overview business analytics dashboard

--- src/containers/BusinessAnalytics/Dashboard/index.jsx
+++ src/containers/BusinessAnalytics/Dashboard/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 import { StyledDashboard } from './index.style';
 
@@ -9,26 +10,29 @@ import { overviews } from './data';
 const Dashboard = () => (
   <StyledDashboard>
     <div className="header">
-      <Grid container spacing={4} justifyContent="space-between">
+      <Grid justifyContent="space-between" alignItems="center" display="flex">
         {overviews.map((item) => (
-          <Grid item xs={3} sm={2} xl={1}>
-            <Box
-              sx={{
-                width: 150,
-                height: 100,
-                backgroundColor: 'primary.dark',
-                '&:hover': {
-                  backgroundColor: 'primary.main',
-                  opacity: [0.9, 0.8, 0.7],
-                },
-              }}
-            >
-              {item.name}
-            </Box>
-          </Grid>
+          <Box className="overview-item">
+            <Typography className="text">{item.name}</Typography>
+            <Typography className="text" variant="h4" fontWeight="500">
+              {item.value}
+            </Typography>
+            <Typography className="text percent-growth-increment">
+              {item.percentGrowth > 0 ? '+' : '-'}
+              {item.percentGrowth}%
+            </Typography>
+          </Box>
         ))}
       </Grid>
     </div>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={12} md={6} lg={8}>
+        <div className="main-item">Revenue</div>
+      </Grid>
+      <Grid item xs={12} sm={12} md={6} lg={4}>
+        <div className="main-item">Top Selling Product</div>
+      </Grid>
+    </Grid>
   </StyledDashboard>
 );
 
